303f61fac1d851ec6fb44ce207286d67bcd03240
s3nt3nz4 <thmscgnx@gmail.com>
2021-05-13 21:08:36 +0200

    responsive

--- sketch.js
+++ sketch.js
@@ -1,12 +1,15 @@
 var song;
 var amp;
+var w = window.innerWidth;
+var h = window.innerHeight;
+
 
 function preload() {
 	song = loadSound("data/Brut192.mp3");
 }
 
 function setup() {
-	var canva = createCanvas(414, 736);
+	canva = createCanvas(w, h);
 
 	canva.mousePressed(playPause);
 
@@ -17,27 +20,27 @@ function draw() {
 	background(0);
 
 	var vol = amp.getLevel();
-	var side1 = map(vol, 0, 0.5, 10, 414);
-	var side2 = map(vol, 0, 0.5, 10, 736);
+	var side1 = map(vol, 0, 0.5, 10, w);
+	var side2 = map(vol, 0, 0.5, 10, h);
 
 	textAlign(LEFT, TOP);
 	textFont('Helvetica');
 	stroke(255);
 	textSize(10);
 
-	if (song.isPlaying()) {
-		text('Pause BRUT', 10, 10);
-	} else {
-		text('Play BRUT', 10, 10);
-	}
+	/* 	if (song.isPlaying()) {
+			text('Pause BRUT', 10, 10);
+		} else {
+			text('Play BRUT', 10, 10);
+		} */
 
 	fill(255);
 	rectMode(RADIUS);
-	rect(width / 2, height / 2, side1, side2);
+	rect(w / 2, h / 2, side1, side2);
 
 	if (vol > 0.3) {
 		fill(0);
-		rect(width / 2, height / 2, side1 / 2, side2 / 2);
+		rect(w / 2, h / 2, side1 / 2, side2 / 2);
 	}
 }
 
@@ -47,4 +50,11 @@ function playPause() {
 	} else {
 		song.play();
 	}
+}
+
+window.onresize = function () {
+	// assigns new values for width and height variables
+	w = window.innerWidth;
+	h = window.innerHeight;
+	canvas.size(w, h);
 }
